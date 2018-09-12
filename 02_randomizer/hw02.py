b48684e252d4054c8c340d633e502e465797b95d
Sajed Nahian <snahian@stuy.edu>
2018-09-12 19:34:25 -0400

    Fixed file structure v2

---
+++ 02_randomizer/hw02.py
@@ -0,0 +1,31 @@
+#Sajed Nahian
+#SoftDev1 pd6
+#K02 NO-body expects the Spanish Inquisition!
+#2018-09-11
+
+import random
+#allows us to use functions for randomization
+
+KREWES = {
+
+        'w': ['William Lu', 'Qian', 'Peter', 'Ahnaf', 'Kenny', 'Sophia', 'Sajed', 'Emily', 'Hasif', 'Brian ', 'Dennis', 'Jiayang', 'Shafali ', 'Isaac ', 'Tania', 'Derek', 'Shin', 'Vincent', 'Ricky', 'Puneet', 'Wei Wen', 'Tim', 'Jeffrey', 'Joyce ', 'Mohtasim', 'Simon', 'Thomas', 'Ray', 'Jack', 'Karen', 'Robin', 'Jabir', 'Johnny ', 'Matthew', 'Johnson Li', 'Angela', 'Crystal', 'Jiajie', 'Theodore (Dont really care)', 'Anton', 'Max', 'Bo', 'Andrew', 'Kendrick', 'Kevin', 'Kyle', 'Jamil', 'Mohammed', 'Ryan', 'Jason'],
+
+        'm': ['Daniel', 'Aleksandra', 'Addison', 'Hui Min', 'Aaron', 'Rubin', 'Raunak', 'Stefan', 'Cheryl', 'Cathy', 'Mai', 'Claire ', 'Alex', 'Bill', 'Daniel', 'Jason'],
+
+        'x': ['Derek', 'Britni', 'Joan', 'Vincent', 'Jared', 'Ivan', 'Thomas', 'Maggie', 'Damian', 'Tina', 'Fabiha', 'John', 'Susan ', 'Kaitlin', 'Michelle', 'Clara', 'Rachel', 'Amit', 'Jerry', 'Raymond', 'Zane', 'Soojin', 'Maryann', 'Adil', 'Josh', 'Imad']
+
+}
+
+teams = ['w','m','x']
+
+def pickRandomStudent():
+    choice=""
+
+    #keeps looping until user enters valid input
+    while (not(choice in teams)):
+        choice = input("Choose a team (w,m, or x): ")
+
+    students = KREWES[choice]
+    print (students[random.randint(0,len(students)-1)])
+            
+pickRandomStudent()

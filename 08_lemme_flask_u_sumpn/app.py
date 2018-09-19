12b37540d12e42840d7ffd600034d8c76b4982bd
snahian <contactsajednahian@gmail.com>
2018-09-19 15:17:12 -0400

    Added latest h.w

---
+++ 08_lemme_flask_u_sumpn/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+app = Flask(__name__) # create instance of class Flask
+
+header = '''
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+		<a href="/contact">Contact</a>
+'''
+
+@app.route("/") #assign fxn to route
+def home_page():
+    print (__name__)
+    return header + "<h1>Home Page</h1>"
+
+@app.route("/about") #assign fxn to route
+def about_page():
+    print (__name__)
+    return header + "<h1>About Page</h1>"
+
+
+@app.route("/contact") #assign fxn to route
+def contact_page():
+    print (__name__)
+    return header + "<h1>Contact Page</h1>"
+
+app.debug = True
+app.run()

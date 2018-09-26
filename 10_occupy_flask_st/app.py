c208972ae1d57cd1d1cd26d29c1dea3ff2da9477
Sajed Nahian <snahian@stuy.edu>
2018-09-26 12:28:24 -0400

    Updated files

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -1,10 +1,10 @@
-#Sajed Nahian
+#Brown Boys - Sajed Nahian Ahnaf Kazi
 #SoftDev1 pd6
 #K10: Jinja Tuning
 #2018-09-24
 
 from flask import Flask, render_template
-import hw06
+from util import hw06
 
 app = Flask(__name__) # create instance of class Flask
 
@@ -12,6 +12,10 @@ app = Flask(__name__) # create instance of class Flask
 @app.route("/occupations") #assign fxn to route
 def occupations_page():
     return render_template('occupations.html', headings = hw06.headings, randomlySelectedJob = hw06.pickRandomOccupation(), occupations = hw06.occupationsDictionary)
+
+@app.route("/")
+def home_page():
+	return "<a href = '/occupations'>Occupations Page</a>"
 
 if __name__ == "__main__":
     app.debug = True

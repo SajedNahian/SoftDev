648bd2380b7a943e832024fec381b8d94245b538
snahian <contactsajednahian@gmail.com>
2018-09-30 18:29:42 -0400

    hw 13 with post

---
+++ 13_formation-post/app.py
@@ -0,0 +1,21 @@
+#Sajed Nahian
+#SoftDev1 pd6
+#K13: Echo Echo Echo
+#2018-09-27
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__) # create instance of class Flask
+
+
+@app.route("/") #assign fxn to route
+def input_field_page():
+    return render_template('InputFields.html')
+
+@app.route("/auth", methods=['POST'])
+def auth_page():	
+	return render_template('Authorize.html', username = request.form['username'], method = request.method)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()

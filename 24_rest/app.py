d202f81802bd56ddd5873d13352c5e805562599a
snahian <contactsajednahian@gmail.com>
2018-11-13 21:11:45 -0500

    refactored code

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -8,12 +8,12 @@ app.secret_key = os.urandom(32)
 
 @app.route("/", methods = ["POST", "GET"])
 def input_field_page():
-	videoUrl = ''
 	# Please be responsible with my public key :)
-	with urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key=E5cEaWdPE4Edf7N0luy673hxUFYvIJCAAPcGmPQN') as url:
-		 data = json.loads(url.read().decode())
-		 videoUrl = data['url']
-	return render_template('NASAPage.html', youTubeVideoSource = videoUrl)
+	request = urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key=E5cEaWdPE4Edf7N0luy673hxUFYvIJCAAPcGmPQN')
+	data = json.loads(request.read())
+	videoUrl = data['url']
+	explanation = data['explanation']
+	return render_template('NASAPage.html', youTubeVideoSource = videoUrl, explanationText = explanation)
 
 if __name__ == "__main__":
 	app.debug = True
